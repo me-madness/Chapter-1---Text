efc271544c96133a7a2f09f49afefd0c858741aa
me-madness <58656549+me-madness@users.noreply.github.com>
2023-11-13 22:35:51 +0100

    done 1.63

--- 1.4 diffib: Compare Sequences/1.63: difflib_data.py
+++ 1.4 diffib: Compare Sequences/1.63: difflib_data.py
@@ -6,8 +6,22 @@ explicabo nobis ab ratione quisquam, deserunt facilis quas accusantium
 repellendus, delectus earum provident et similique harum quis ducimus? 
 Dolor, quod delectus at beatae aperiam reiciendis expedita exercitationem 
 excepturi minus sapiente ducimus architecto dolore velit, eos inventore voluptatibus 
+soluta quos eveniet doloremque omnis.
+"""
+
+rext1_lines = text1.splitlines()
+
+text2 = """Lorem ipsum, dolor sit amet consectetur adipisicing 
+elit. Sapiente numquam velit amet doloremque ad quasi autem itaque 
+voluptatibus rerum illum tempora adipisci similique, consectetur, 
+id ea iure. Voluptate id maxime, fugiat illum totam eveniet laboriosam
+explicabo nobis ab ratione quisquam, deserunt facilis quas accusantium 
+repellendus, delectus earum provident et similique harum quis ducimus? 
+Dolor, quod delectus at beatae aperiam reiciendis expedita exercitationem 
+excepturi minus sapiente ducimus architecto dolore velit, eos inventore voluptatibus 
 soluta quos eveniet doloremque omnis. Maxime ab, modi natus iusto alias, iste quam accusamus 
 veritatis non saepe voluptatem quae labore dolores facere numquam quibusdam temporibus, 
 accusantium officia hic.
 """
 
+rext2_lines = text2.splitlines()
